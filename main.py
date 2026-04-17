60c04963df04318211030facdc94e4a8293b5287
Celia <celiaailec745@gmail.com>
2026-04-17 19:05:07 +0100

    fix jinja TemplateResponse

--- main.py
+++ main.py
@@ -15,11 +15,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        context={"request": request}
-    )
-    # return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/predict")
 def predict(data: ModelInput):
